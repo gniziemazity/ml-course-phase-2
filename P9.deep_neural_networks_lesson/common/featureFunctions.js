d0c90e89a6da69da37ef4a5f262a4b7ffee22efa
Radu Mariescu-Istodor <gniziemazity@gmail.com>
2023-07-05 07:53:32 +0300

    Add files via upload

---
+++ P9.deep_neural_networks_lesson/common/featureFunctions.js
@@ -0,0 +1,123 @@
+if (typeof geometry === "undefined") {
+   geometry = require("./geometry.js");
+}
+if (typeof draw === "undefined") {
+   draw = require("./draw.js");
+}
+
+const featureFunctions = {};
+
+featureFunctions.getPathCount = (paths) => {
+   return paths.length;
+};
+
+featureFunctions.getPointCount = (paths) => {
+   const points = paths.flat();
+   return points.length;
+};
+
+featureFunctions.getWidth = (paths) => {
+   const points = paths.flat();
+   if (points.length == 0) {
+      return 0;
+   }
+   const x = points.map((p) => p[0]);
+   const min = Math.min(...x);
+   const max = Math.max(...x);
+   return max - min;
+};
+
+featureFunctions.getHeight = (paths) => {
+   const points = paths.flat();
+   if (points.length == 0) {
+      return 0;
+   }
+   const y = points.map((p) => p[1]);
+   const min = Math.min(...y);
+   const max = Math.max(...y);
+   return max - min;
+};
+
+featureFunctions.getElongation = (paths) => {
+   const points = paths.flat();
+   const { width, height } = geometry.minimumBoundingBox({ points });
+   return ( Math.max(width, height) + 1) / 
+          ( Math.min(width, height) + 1);
+};
+
+featureFunctions.getRoundness = (paths) => {
+   const points = paths.flat();
+   const { hull } = geometry.minimumBoundingBox({ points });
+   return geometry.roundness(hull);
+};
+
+featureFunctions.getPixels = (paths, size = 400, expand = true) => {
+   let canvas = null;
+
+   try {
+      // for web
+      canvas = document.createElement("canvas");
+      canvas.width = size;
+      canvas.height = size;
+   } catch (err) {
+      // for node
+      const { createCanvas } = require("../node/node_modules/canvas");
+      canvas = createCanvas(size, size);
+   }
+
+   const ctx = canvas.getContext("2d");
+
+   if (expand) {
+      const points = paths.flat();
+
+      const bounds = {
+         left: Math.min(...points.map((p) => p[0])),
+         right: Math.max(...points.map((p) => p[0])),
+         top: Math.min(...points.map((p) => p[1])),
+         bottom: Math.max(...points.map((p) => p[1]))
+      };
+
+      const newPaths = [];
+      for (const path of paths) {
+         const newPoints = path.map(p => 
+            [
+               utils.invLerp(bounds.left, bounds.right, p[0]) * size,
+               utils.invLerp(bounds.top, bounds.bottom, p[1]) * size
+            ]
+         );
+         newPaths.push(newPoints);
+      }
+      draw.paths(ctx, newPaths);
+   } else {
+      draw.paths(ctx, paths);
+   }
+
+   const imgData = ctx.getImageData(0, 0, size, size);
+   return imgData.data.filter((val, index) => index % 4 == 3);
+};
+
+featureFunctions.getComplexity = (paths) => {
+   const pixels = featureFunctions.getPixels(paths);
+   return pixels.filter((a) => a != 0).length;
+};
+
+featureFunctions.inUse = [
+   { 
+      name: "Pixel Array", function: (paths) => {
+         return featureFunctions.getPixels(paths, 20) 
+      }
+   }
+   //{name:"Path Count",function:featureFunctions.getPathCount},
+   //{name:"Point Count",function:featureFunctions.getPointCount},
+   /*
+   { name: "Width", function: featureFunctions.getWidth },
+   { name: "Height", function: featureFunctions.getHeight },
+   { name: "Elongation", function: featureFunctions.getElongation },
+   { name: "Roundness", function: featureFunctions.getRoundness },
+   { name: "Complexity", function: featureFunctions.getComplexity },
+   */
+];
+
+if (typeof module !== "undefined") {
+   module.exports = featureFunctions;
+}
---
+++ P9.deep_neural_networks_lesson/node/feature_extractor.js
@@ -0,0 +1,104 @@
+const constants = require("../common/constants.js");
+const featureFunctions = require("../common/featureFunctions.js");
+const utils = require("../common/utils.js");
+
+const fs = require("fs");
+
+console.log("EXTRACTING FEATURES ...");
+
+const samples = JSON.parse(fs.readFileSync(constants.SAMPLES));
+
+for (let i = 0; i < samples.length; i++) {
+   const sample = samples[i];
+   const paths = JSON.parse(
+      fs.readFileSync(constants.JSON_DIR + "/" + sample.id + ".json")
+   );
+   const functions = featureFunctions.inUse.map((f) => f.function);
+   //sample.point = functions.map((f) => f(paths));
+   sample.point = Object.values(functions[0](paths));
+   utils.printProgress(i, samples.length - 1);
+}
+
+//const featureNames = featureFunctions.inUse.map((f) => f.name);
+const featureNames = Array(samples[0].point.length).fill(" ");
+
+console.log("GENERATING SPLITS ...");
+
+const trainingAmount = samples.length * 0.5;
+
+const training = [];
+const testing = [];
+for (let i = 0; i < samples.length; i++) {
+   if (i < trainingAmount) {
+      training.push(samples[i]);
+   } else {
+      testing.push(samples[i]);
+   }
+}
+
+const minMax = utils.normalizePoints(training.map((s) => s.point));
+utils.normalizePoints(
+   testing.map((s) => s.point),
+   minMax
+);
+
+// Output
+
+fs.writeFileSync(
+   constants.TRAINING,
+   JSON.stringify({
+      featureNames,
+      samples: training.map((s) => {
+         return {
+            point: s.point,
+            label: s.label,
+         };
+      }),
+   })
+);
+
+fs.writeFileSync(
+   constants.TRAINING_JS,
+   `const training = ${JSON.stringify({ featureNames, samples: training })};`
+);
+
+fs.writeFileSync(
+   constants.TRAINING_CSV,
+   utils.toCSV(
+      [...featureNames, "Label"],
+      training.map((a) => [...a.point, a.label])
+   )
+);
+
+fs.writeFileSync(
+   constants.TESTING,
+   JSON.stringify({
+      featureNames,
+      samples: testing.map((s) => {
+         return {
+            point: s.point,
+            label: s.label,
+         };
+      }),
+   })
+);
+
+fs.writeFileSync(
+   constants.TESTING_JS,
+   `const testing = ${JSON.stringify({ featureNames, samples: testing })};`
+);
+
+fs.writeFileSync(
+   constants.TESTING_CSV,
+   utils.toCSV(
+      [...featureNames, "Label"],
+      testing.map((a) => [...a.point, a.label])
+   )
+);
+
+fs.writeFileSync(
+   constants.MIN_MAX_JS,
+   `const minMax = ${JSON.stringify(minMax)};`
+);
+
+console.log("DONE!\n");
